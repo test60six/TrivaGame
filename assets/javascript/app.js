868f87aaa97f7a5af1c71ad4540fc30cfa07e8fd
test60six <test60six@gmail.com>
2017-08-16 16:14:00 -0400

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+console.log("test")
+
+var questions = [ "Which member of Pink Floyd died in 2008?","How did Freddie Mercury die?","What Jimi Hendrix song says  'Scuse me while I kiss the sky?'",
+"Who was the first singer for Pink Floyd?","what was the real name of the Beatles' drummer?","How many instruments could David Bowie play?",
+]
+//need to separate questions and answers//
+var answers = ["A.Roger Waters,B.Rick Wright,C.Nick Mason D.David Gilmour","A.AIDS,B.Cancer,C.Drug Overdose,D.Alcohol Poisoning","•	A.Foxy Lady,B.The Wind Cries Mary,C.Purple Haze,D.Castles Made of Sand,","A.Roger Waters,B.Syd Barret,C.David Gilmour, D.Rick Wright"," A.Richard Starr,B.Rick Starrsky,C.Ringo Starr,D.Richard Stareky",
+"•	A.3,B.4,C.2,D.5"
+]
+
+var correctAnswers ["Rick Wright","A.AIDS","C.Purple Haze","B. Syd Barret","C. Ringo Starr","B.4"
+
+] 
+
+
+
+//STUCK ON HOW//
+//I have a tutor now//
+
+//I need a 30s timer//
+//onclick for start timer and load questions sheet//
+button.onclick(){
+	
+}
+
+//radio buttons for answers//
+//done button to submit answers//
+//method for calculating results//
+
+
+
+
+
+
